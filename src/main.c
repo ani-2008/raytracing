c6e8750e6a4fb1348f03d7bfb4a9de04ad2a1673
bitabyte <anirudan2008@gmail.com>
2026-02-22 07:33:46 +0530

    using vec3.h and color.h

--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+#include "vec3.h" 
+#include "color.h"
+
 #include <stdio.h>
 
 int main()
@@ -8,14 +11,11 @@ int main()
     printf("P3\n%d %d\n255\n",img_width,img_height);
     for(int j = 0; j < img_height; j++){
         for(int i = 0; i < img_width; i++){
-            long double r = (double)i / (img_width-1);
-            long double g = (double)j / (img_height-1);
-            long double b = 0.0;
-            int ir = (int)(255.999 * r);
-            int ig = (int)(255.999 * g);
-            int ib = (int)(255.999 * b);
+            color pixel_color;
+            
+            vec3_put(&pixel_color, (double) i / (img_width-1), (double)j / (img_height-1), 0);
 
-            printf("%d %d %d\n",ir,ig,ib);
+            write_color(&pixel_color);
         }
     }
 
