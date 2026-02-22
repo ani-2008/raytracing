83b9929db401d6941ef8233e64a8ccca0b983c42
bitabyte <anirudan2008@gmail.com>
2026-02-22 07:31:31 +0530

    color.h

---
+++ src/color.h
@@ -0,0 +1,25 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <stdio.h>
+
+typedef vec3 color;
+
+static inline void write_color(const color *pixel_color)
+{
+    double r = vec3_x(pixel_color);
+    double g = vec3_y(pixel_color);
+    double b = vec3_z(pixel_color);
+
+    int rbyte = (int) (255.999 * r);
+    int gbyte = (int) (255.999 * g);
+    int bbyte = (int) (255.999 * b);
+
+    printf("%d %d %d\n", rbyte, gbyte, bbyte);
+
+}
+
+#endif
+
