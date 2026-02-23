490f489cffbb990bbf76ad77a7a1e51bc7420cae
bitabyte <anirudan2008@gmail.com>
2026-02-23 21:40:06 +0530

    clean up

--- src/main.c
+++ src/main.c
@@ -11,9 +11,9 @@ int main()
     printf("P3\n%d %d\n255\n",img_width,img_height);
     for(int j = 0; j < img_height; j++){
         for(int i = 0; i < img_width; i++){
-            color pixel_color;
+            Color pixel_color;
             
-            vec3_put(&pixel_color, (double) i / (img_width-1), (double)j / (img_height-1), 0);
+            vec3(&pixel_color, (double) i / (img_width-1), (double)j / (img_height-1), 0);
 
             write_color(&pixel_color);
         }
--- src/vec3.h
+++ src/vec3.h
@@ -6,38 +6,31 @@
 
 typedef struct{
     double e[3];
-} vec3;
+} Vec3;
 
-static inline void vec3_init(vec3 *v)
-{
-    v->e[0] = 0;
-    v->e[1] = 0; 
-    v->e[2] = 0;
-}
-
-static inline void vec3_put(vec3 *v, double e0, double e1, double e2)
+static inline void vec3(Vec3 *v, double e0, double e1, double e2)
 {
     v->e[0] = e0;
     v->e[1] = e1;
     v->e[2] = e2;
 }
 
-static inline double vec3_x(const vec3 *v)
+static inline double vec3_x(const Vec3 *v)
 {
     return v->e[0];
 }
 
-static inline double vec3_y(const vec3 *v)
+static inline double vec3_y(const Vec3 *v)
 {
     return v->e[1];
 }
 
-static inline double vec3_z(const vec3 *v)
+static inline double vec3_z(const Vec3 *v)
 {
     return v->e[2];
 }
 
-static inline vec3 *vec3_add_assign(vec3 *v, const vec3 *v1)
+static inline Vec3 *vec3_add_assign(Vec3 *v, const Vec3 *v1)
 {
     v->e[0] += v1->e[0];
     v->e[1] += v1->e[1];
@@ -47,7 +40,7 @@ static inline vec3 *vec3_add_assign(vec3 *v, const vec3 *v1)
 }
 
 
-static inline vec3 *vec3_mul_assign(vec3 *v, double t)
+static inline Vec3 *vec3_mul_assign(Vec3 *v, double t)
 {
     v->e[0] *= t;
     v->e[1] *= t;
@@ -56,90 +49,90 @@ static inline vec3 *vec3_mul_assign(vec3 *v, double t)
     return v;
 }
 
-static inline vec3 *vec3_div_assign(vec3 *v, double t)
+static inline Vec3 *vec3_div_assign(Vec3 *v, double t)
 {
     return vec3_mul_assign(v,1.0/t);
 }
 
-static inline double vec3_length_squared(const vec3 *v)
+static inline double vec3_length_squared(const Vec3 *v)
 {
     return (v->e[0] * v->e[0]) + (v->e[1] * v->e[1]) + (v->e[2] * v->e[2]);
 }
 
-static inline double vec3_length(const vec3 *v)
+static inline double vec3_length(const Vec3 *v)
 {
     return sqrt(vec3_length_squared(v));
 }
 
-typedef vec3 point3;
+typedef Vec3 Point3;
 
-static inline void vec3_print(const vec3 *v)
+static inline void vec3_print(const Vec3 *v)
 {
     printf("%f %f %f\n", v->e[0], v->e[1], v->e[2]);
 }
 
-static inline vec3 vec3_add(const vec3 *u, const vec3 *v)
+static inline Vec3 vec3_add(const Vec3 *u, const Vec3 *v)
 {
-    vec3 *x;
+    Vec3 *x;
 
-    vec3_put(x, (u->e[0] + v->e[0]), (u->e[1] + v->e[1]), (u->e[2] + v->e[2]));
+    vec3(x, (u->e[0] + v->e[0]), (u->e[1] + v->e[1]), (u->e[2] + v->e[2]));
 
     return *x;
 }
 
-static inline vec3 vec3_sub(const vec3 *u, const vec3 *v)
+static inline Vec3 vec3_sub(const Vec3 *u, const Vec3 *v)
 {
-    vec3 *x;
+    Vec3 *x;
 
-    vec3_put(x, (u->e[0] - v->e[0]), (u->e[1] - v->e[1]), (u->e[2] - v->e[2]));
+    vec3(x, (u->e[0] - v->e[0]), (u->e[1] - v->e[1]), (u->e[2] - v->e[2]));
 
     return *x;
 }
 
-static inline vec3 vec3_mul(const vec3 *u, const vec3 *v)
+static inline Vec3 vec3_mul(const Vec3 *u, const Vec3 *v)
 {
-    vec3 *x;
+    Vec3 *x;
 
-    vec3_put(x, (u->e[0] * v->e[0]), (u->e[1] * v->e[1]), (u->e[2] * v->e[2]));
+    vec3(x, (u->e[0] * v->e[0]), (u->e[1] * v->e[1]), (u->e[2] * v->e[2]));
 
     return *x;
 }
 
-static inline vec3 vec3_mul_t(double t, const vec3 *v)
+static inline Vec3 vec3_mul_t(double t, const Vec3 *v)
 {
-    vec3 *x;
+    Vec3 *x;
 
-    vec3_put(x, (t * v->e[0]), (t * v->e[1]), (t * v->e[2]));
+    vec3(x, (t * v->e[0]), (t * v->e[1]), (t * v->e[2]));
 
     return *x;
 }
 
-static inline vec3 vec3_div(const vec3 *v, double t)
+static inline Vec3 vec3_div(const Vec3 *v, double t)
 {
-    vec3 *x;
+    Vec3 *x;
 
     vec3_mul_t((1.0/t), v);
 
     return *x;
 }
 
-static inline double dot(const vec3 *u, const vec3 *v)
+static inline double dot(const Vec3 *u, const Vec3 *v)
 {
     return (u->e[0] * v->e[0]
           + u->e[1] * v->e[1]
           + u->e[2] * v->e[2]);
 }
 
-static inline vec3 cross(const vec3 *u, const vec3 *v)
+static inline Vec3 cross(const Vec3 *u, const Vec3 *v)
 {
-    vec3 *x;
-    vec3_put(x, u->e[1] * v->e[2] - u->e[2] * v->e[1],
+    Vec3 *x;
+    vec3(x, u->e[1] * v->e[2] - u->e[2] * v->e[1],
                 u->e[0] * v->e[2] - u->e[2] * v->e[0],
                 u->e[0] * v->e[1] - u->e[1] * v->e[0]);
     return *x;
 }
 
-static inline vec3 unit_vector(const vec3 *v)
+static inline Vec3 unit_vector(const Vec3 *v)
 {
     return vec3_div(v, vec3_length(v));
 }
